04d10f2d29f6438776795ca2a02e95152a317849
YangKai5t5 <test7t7@gmail.com>
2024-09-23 14:57:39 +0530

    third commit

---
+++ Main/1/1.cpp
@@ -0,0 +1,23 @@
+// reverse digits of given number.
+
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n)
+{
+    int nReturn = 0;
+    while (n != 0)
+    {
+        int lastDigit = n % 10;
+        nReturn = nReturn * 10 + lastDigit;
+        n /= 10;
+    }
+    return nReturn;
+}
+
+int main()
+{
+    int number = 45;
+    int reversed = reverseNumber(number);
+    cout << reversed << endl;
+}
