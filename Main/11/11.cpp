1455fee1bf9debed4c8da2fa56837090f96a7db6
YangKai5t5 <test7t7@gmail.com>
2024-09-28 23:22:29 +0530

    1

---
+++ Main/11/11.cpp
@@ -0,0 +1,8 @@
+// Second highes number without sorting
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+}
--- Main/7/7.cpp
+++ Main/7/7.cpp
@@ -1,67 +1,21 @@
-// find unique element in array using O(n) time and O(1) space complexity.
+// Find the kth highest element in an array
+
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
-// using xor for complexity condition
-
-// Properties of XOR:
-// x ^ x = 0 (Any number XORed with itself is 0)
-// x ^ 0 = x (Any number XORed with 0 is the number itself)
-// Pairs of identical elements will cancel each other out (because x ^ x = 0).
-// The unique element will be left as the result (because x ^ 0 = x).
-
-// For the array {1, 2, 3, 2, 1}:
-
-// Initial unique = 0
-// After first iteration: unique = 0 ^ 1 = 1
-// After second iteration: unique = 1 ^ 2 = 3
-// After third iteration: unique = 3 ^ 3 = 0
-// After fourth iteration: unique = 0 ^ 2 = 2
-// After fifth iteration: unique = 2 ^ 1 = 3
-// So, the unique element in the array is 3.
-int findUniqueElement(int arr[], int n)
-{
-    int unique = 0;
-    for (int i = 0; i < n; i++)
-    {
-        unique ^= arr[i];
-    }
-    return unique;
-}
 
 int main()
 {
-    int arr[] = {1, 2, 3, 2, 1};
+    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int uniqueElement = findUniqueElement(arr, n);
-    cout << "The unique element in the array is: " << uniqueElement << endl;
+    int k = 5;
+
+    sort(arr, arr + n, greater<int>());
+
+    int kth_highest = arr[k - 1];
+
+    cout << endl
+         << "The number" << " " << k << " " << "highest element is " << kth_highest << endl;
+
     return 0;
 }
-// using hashmap
-//  void findUniqueElements(int arr[], int n)
-//  {
-//      unordered_map<int, int> unq;
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         unq[arr[i]]++;
-//     }
-
-//     cout << "Unique elements in the array: ";
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (unq[arr[i]] == 1)
-//         {
-//             cout << arr[i] << " ";
-//         }
-//     }
-//     cout << endl;
-// }
-
-// int main()
-// {
-//     int arr[] = {1, 2, 1, 3, 2, 4, 5};
-//     int n = sizeof(arr) / sizeof(arr[0]);
-//     findUniqueElements(arr, n);
-//     return 0;
-// }
