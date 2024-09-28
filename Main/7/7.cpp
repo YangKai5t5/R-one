9576fa13938b1ad0512a6a9a37dee3578d4b0848
YangKai5t5 <test7t7@gmail.com>
2024-09-28 16:42:02 +0530

    1

--- Main/7/7.cpp
+++ Main/7/7.cpp
@@ -2,7 +2,23 @@
 #include <iostream>
 #include <unordered_map>
 using namespace std;
-//using xor for complexity condition
+// using xor for complexity condition
+
+// Properties of XOR:
+// x ^ x = 0 (Any number XORed with itself is 0)
+// x ^ 0 = x (Any number XORed with 0 is the number itself)
+// Pairs of identical elements will cancel each other out (because x ^ x = 0).
+// The unique element will be left as the result (because x ^ 0 = x).
+
+// For the array {1, 2, 3, 2, 1}:
+
+// Initial unique = 0
+// After first iteration: unique = 0 ^ 1 = 1
+// After second iteration: unique = 1 ^ 2 = 3
+// After third iteration: unique = 3 ^ 3 = 0
+// After fourth iteration: unique = 0 ^ 2 = 2
+// After fifth iteration: unique = 2 ^ 1 = 3
+// So, the unique element in the array is 3.
 int findUniqueElement(int arr[], int n)
 {
     int unique = 0;
@@ -21,10 +37,10 @@ int main()
     cout << "The unique element in the array is: " << uniqueElement << endl;
     return 0;
 }
-//using hashmap
-// void findUniqueElements(int arr[], int n)
-// {
-//     unordered_map<int, int> unq;
+// using hashmap
+//  void findUniqueElements(int arr[], int n)
+//  {
+//      unordered_map<int, int> unq;
 
 //     for (int i = 0; i < n; i++)
 //     {
