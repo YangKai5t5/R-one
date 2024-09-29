74d88c36bc458ab92450bce5ab4681c2f2f1bfb8
YangKai5t5 <test7t7@gmail.com>
2024-09-29 11:58:48 +0530

    1

--- Main/11/11.cpp
+++ Main/11/11.cpp
@@ -3,6 +3,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int findSecondHighest(int arr[], int n)
+{
+    int first = INT_MIN, second = INT_MIN;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (arr[i] > first)
+        {
+            second = first;
+            first = arr[i];
+        }
+        else if (arr[i] > second && arr[i] < first)
+        {
+            second = arr[i];
+        }
+    }
+
+    return (second == INT_MIN) ? -1 : second; // Return -1 if there is no second highest number
+}
+
+int main()
+{
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int secondHighest = findSecondHighest(arr, n);
+    if (secondHighest != -1)
+    {
+        cout << "The second highest number is " << secondHighest << endl;
+    }
+    else
+    {
+        cout << "There is no second highest number." << endl;
+    }
+    return 0;
 }
